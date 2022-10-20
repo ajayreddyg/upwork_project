9557d7d5d5317a87829b9f8e93c9b80abc225fb1
ajayreddyg <ajayreddy13102000@gmail.com>
2022-10-20 13:59:52 +0530

    initial

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="flex_container">
+        <h1 className="title">
+          <span>BY</span>ME
+          <sup> &reg;</sup>
+        </h1>
+        <p className="sub_title">order almost anything</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
