ffbefa2df5ce17d3d73474d81983005c201029fe
prudhvic <prudhvichinnam101@gmail.com>
2022-10-20 14:27:26 +0530

    style changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-
+import { BsChevronRight } from "react-icons/bs";
 const Home = () => {
   return (
     <div className="container">
+      <div className="arrows">
+        <BsChevronRight />
+        <BsChevronRight />
+      </div>
       <div className="flex_container">
-        <h1 className="title">
-          <span>BY</span>ME
-          <sup> &reg;</sup>
-        </h1>
-        <p className="sub_title">order almost anything</p>
+      
+      <h1 className="title">
+        <span>BY</span>ME
+        <sup> &reg;</sup>
+      </h1>
+      <p className="sub_title">order almost anything</p>
       </div>
     </div>
   );
